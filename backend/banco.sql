0f9904b4311e809d2efcff639721e21a597f59ed
danisantosss <danisantos5075@gmail.com>
2025-03-23 21:36:20 -0300

    comandos sql

--- backend/banco.sql
+++ backend/banco.sql
@@ -1,4 +1,6 @@
 USE gerenciador_escolar;
 
-INSERT INTO aluno (nome, cpf, data_nascimento) 
-VALUES ('Daniel', '123.456.789-00', '2005-03-15');
+INSERT INTO turma (nome, ano_letivo) VALUES ('Turma A', 2024);
+
+INSERT INTO aluno (nome, cpf, data_nascimento, turma_id) 
+VALUES ('Daniel', '123.456.789-00', '2005-03-15', 1);
