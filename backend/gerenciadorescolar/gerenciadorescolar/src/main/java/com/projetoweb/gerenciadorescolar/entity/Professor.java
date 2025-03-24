d3325f165a24ed0e0ce0c237ef503d428b7d4168
danisantosss <danisantos5075@gmail.com>
2025-03-23 21:21:47 -0300

    Conexão entre entidade professor e turma

--- backend/gerenciadorescolar/gerenciadorescolar/src/main/java/com/projetoweb/gerenciadorescolar/entity/Professor.java
+++ backend/gerenciadorescolar/gerenciadorescolar/src/main/java/com/projetoweb/gerenciadorescolar/entity/Professor.java
@@ -1,10 +1,14 @@
 package com.projetoweb.gerenciadorescolar.entity;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -12,9 +16,12 @@ import lombok.Data;
 @Table(name = "professor")
 @Entity
 public class Professor {
-     @Id
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String disciplina;
+
+    @ManyToMany(mappedBy = "professores")
+    private List<Turma> turmas = new ArrayList<>();
 }
