a43a3514a2c28542454ce376b7b693a726487692
danisantosss <danisantos5075@gmail.com>
2025-03-23 21:20:53 -0300

    Criação entidade professor

---
+++ backend/gerenciadorescolar/gerenciadorescolar/src/main/java/com/projetoweb/gerenciadorescolar/entity/Professor.java
@@ -0,0 +1,20 @@
+package com.projetoweb.gerenciadorescolar.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Table(name = "professor")
+@Entity
+public class Professor {
+     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String disciplina;
+}
