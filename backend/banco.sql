4910e1d75891f145ce941a4ecfd61ca87e89d80f
danisantosss <danisantos5075@gmail.com>
2025-03-23 21:17:39 -0300

    alteração classe aluno

---
+++ backend/banco.sql
@@ -0,0 +1,4 @@
+USE gerenciador_escolar;
+
+INSERT INTO aluno (nome, cpf, data_nascimento) 
+VALUES ('Daniel', '123.456.789-00', '2005-03-15');
--- backend/gerenciadorescolar/gerenciadorescolar/src/main/java/com/projetoweb/gerenciadorescolar/entity/Aluno.java
+++ backend/gerenciadorescolar/gerenciadorescolar/src/main/java/com/projetoweb/gerenciadorescolar/entity/Aluno.java
@@ -1,5 +1,7 @@
 package com.projetoweb.gerenciadorescolar.entity;
 
+import java.util.Date;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -14,4 +16,6 @@ public class Aluno {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
+    private String cpf;
+    private Date dataNascimento;
 }
