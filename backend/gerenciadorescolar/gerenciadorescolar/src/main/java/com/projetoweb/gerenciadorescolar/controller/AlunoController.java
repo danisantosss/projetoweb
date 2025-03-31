3d005e12a1e11cf3ba4dfada353ddfde711effb4
danisantosss <danisantos5075@gmail.com>
2025-03-31 20:00:43 -0300

    atualização

--- backend/gerenciadorescolar/gerenciadorescolar/src/main/java/com/projetoweb/gerenciadorescolar/controller/AlunoController.java
+++ backend/gerenciadorescolar/gerenciadorescolar/src/main/java/com/projetoweb/gerenciadorescolar/controller/AlunoController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.projetoweb.gerenciadorescolar.entity.Aluno;
 import com.projetoweb.gerenciadorescolar.entity.AlunoDTO;
 import com.projetoweb.gerenciadorescolar.repository.AlunoRepository;
 
