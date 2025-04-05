3b112fa9ee20fe1291f7e0f852ca67eff7afe588
danisantosss <danisantos5075@gmail.com>
2025-04-04 22:32:30 -0300

    turma repository

---
+++ backend/gerenciadorescolar/gerenciadorescolar/src/main/java/com/projetoweb/gerenciadorescolar/repository/TurmaRepository.java
@@ -0,0 +1,9 @@
+package com.projetoweb.gerenciadorescolar.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.projetoweb.gerenciadorescolar.entity.Turma;
+
+public interface TurmaRepository extends JpaRepository <Turma, Long> {
+    
+}
