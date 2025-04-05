b0c287232fee9520e589ffe730edca41b0badb7e
danisantosss <danisantos5075@gmail.com>
2025-04-04 22:31:59 -0300

    professor repository

---
+++ backend/gerenciadorescolar/gerenciadorescolar/src/main/java/com/projetoweb/gerenciadorescolar/repository/ProfessorRepository.java
@@ -0,0 +1,9 @@
+package com.projetoweb.gerenciadorescolar.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.projetoweb.gerenciadorescolar.entity.Professor;
+
+public interface ProfessorRepository extends JpaRepository<Professor, Long> {
+    
+}
