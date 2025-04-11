b7f5ac70ca0da1499d9f11eecba5ab6fdd1ec8fe
danisantosss <danisantos5075@gmail.com>
2025-04-11 20:17:21 -0300

    delete turma

--- backend/gerenciadorescolar/gerenciadorescolar/src/main/java/com/projetoweb/gerenciadorescolar/controller/TurmaController.java
+++ backend/gerenciadorescolar/gerenciadorescolar/src/main/java/com/projetoweb/gerenciadorescolar/controller/TurmaController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -46,5 +47,14 @@ public class TurmaController {
             Turma turmaAtualizada = turmaRepository.save(turma);
             return ResponseEntity.ok(new TurmaDTO(turmaAtualizada));
         }).orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> delete(@PathVariable Long id){
+        if(turmaRepository.existsById(id)){
+            turmaRepository.deleteById(id);
+            return ResponseEntity.ok("Turma deletada com sucesso!");
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Turma não encontrada.");
     }
 }
