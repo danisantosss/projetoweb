f49daf7a2284b622c537642ba87e22845df6db7b
danisantosss <danisantos5075@gmail.com>
2025-04-11 19:57:30 -0300

    delete professor

--- backend/gerenciadorescolar/gerenciadorescolar/src/main/java/com/projetoweb/gerenciadorescolar/controller/ProfessorController.java
+++ backend/gerenciadorescolar/gerenciadorescolar/src/main/java/com/projetoweb/gerenciadorescolar/controller/ProfessorController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -48,5 +49,14 @@ public class ProfessorController {
             Professor professorAtualizado = professorRepository.save(professor);
             return ResponseEntity.ok(new ProfessorDTO(professorAtualizado));
         }).orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> delete(@PathVariable Long id){
+        if (professorRepository.existsById(id)){
+            professorRepository.deleteById(id);
+            return ResponseEntity.ok("Professor deletado com sucesso!");
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Professor não encontrado.");
     }
 }
