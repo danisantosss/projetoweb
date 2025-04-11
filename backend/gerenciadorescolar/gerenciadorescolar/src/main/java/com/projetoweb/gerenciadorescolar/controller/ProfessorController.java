1be151d2139375e60b22b82f8b2e7c2cb81da473
danisantosss <danisantos5075@gmail.com>
2025-04-11 19:49:43 -0300

    update professor

--- backend/gerenciadorescolar/gerenciadorescolar/src/main/java/com/projetoweb/gerenciadorescolar/controller/ProfessorController.java
+++ backend/gerenciadorescolar/gerenciadorescolar/src/main/java/com/projetoweb/gerenciadorescolar/controller/ProfessorController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,5 +34,19 @@ public class ProfessorController {
     public List<ProfessorDTO> getAll() {
         List<ProfessorDTO> listaProfessores = professorRepository.findAll().stream().map(ProfessorDTO::new).toList();
         return listaProfessores;
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProfessorDTO> update(@PathVariable Long id, @RequestBody Professor dados) {
+        return professorRepository.findById(id).map(professor -> {
+            professor.setNome(dados.getNome());
+            professor.setCpf(dados.getCpf());
+            professor.setEmail(dados.getEmail());
+            professor.setDisciplina(dados.getDisciplina());
+            professor.setTurmas(dados.getTurmas());
+
+            Professor professorAtualizado = professorRepository.save(professor);
+            return ResponseEntity.ok(new ProfessorDTO(professorAtualizado));
+        }).orElse(ResponseEntity.notFound().build());
     }
 }
