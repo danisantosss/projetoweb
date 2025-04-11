32b4dae492d413648933ab48074404616f6944d5
danisantosss <danisantos5075@gmail.com>
2025-04-11 20:07:25 -0300

    getall turma

--- backend/gerenciadorescolar/gerenciadorescolar/src/main/java/com/projetoweb/gerenciadorescolar/controller/TurmaController.java
+++ backend/gerenciadorescolar/gerenciadorescolar/src/main/java/com/projetoweb/gerenciadorescolar/controller/TurmaController.java
@@ -1,8 +1,11 @@
 package com.projetoweb.gerenciadorescolar.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,5 +26,11 @@ public class TurmaController {
     public ResponseEntity<TurmaDTO> create (@RequestBody Turma turma){
         Turma novaTurma = turmaRepository.save(turma);
         return ResponseEntity.status(HttpStatus.CREATED).body(new TurmaDTO(novaTurma));
+    }
+
+    @GetMapping
+    public List<TurmaDTO> getAll(){
+        List<TurmaDTO> listaTurmas = turmaRepository.findAll().stream().map(TurmaDTO::new).toList();
+        return listaTurmas;
     }
 }
