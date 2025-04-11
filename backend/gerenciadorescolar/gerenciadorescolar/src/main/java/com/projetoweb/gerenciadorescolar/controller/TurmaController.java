8d2e3eb8c0de1d97e60033ec1e590329c3b04291
danisantosss <danisantos5075@gmail.com>
2025-04-11 20:02:30 -0300

    create turma

--- backend/gerenciadorescolar/gerenciadorescolar/src/main/java/com/projetoweb/gerenciadorescolar/controller/TurmaController.java
+++ backend/gerenciadorescolar/gerenciadorescolar/src/main/java/com/projetoweb/gerenciadorescolar/controller/TurmaController.java
@@ -1,10 +1,27 @@
 package com.projetoweb.gerenciadorescolar.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.projetoweb.gerenciadorescolar.entity.Turma;
+import com.projetoweb.gerenciadorescolar.entity.TurmaDTO;
+import com.projetoweb.gerenciadorescolar.repository.TurmaRepository;
 
 @RestController
 @RequestMapping("turma")
 public class TurmaController {
-    
+
+    @Autowired
+    TurmaRepository turmaRepository;
+
+    @PostMapping
+    public ResponseEntity<TurmaDTO> create (@RequestBody Turma turma){
+        Turma novaTurma = turmaRepository.save(turma);
+        return ResponseEntity.status(HttpStatus.CREATED).body(new TurmaDTO(novaTurma));
+    }
 }
