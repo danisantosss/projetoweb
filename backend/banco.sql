1f5fd3d6b6b6a3a3c2fc05ae1e8e199ea367088b
danisantosss <danisantos5075@gmail.com>
2025-04-11 21:39:06 -0300

    comandos sql

--- backend/banco.sql
+++ backend/banco.sql
@@ -4,3 +4,9 @@ INSERT INTO turma (nome, ano_letivo) VALUES ('Turma A', 2024);
 
 INSERT INTO aluno (nome, cpf, data_nascimento, turma_id) 
 VALUES ('Daniel', '123.456.789-00', '2005-03-15', 1);
+
+INSERT INTO professor (nome, cpf, email, disciplina)
+VALUES ('Luiz Camargo', '123.456.789-00', 'camargo@email.com', 'Programação Web');
+
+INSERT INTO turma_professor (turma_id, professor_id)
+VALUES (1, 1);
