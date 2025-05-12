78a8db24e8c2a0e07ceb08cee3967de6a3754683
danisantosss <danisantos5075@gmail.com>
2025-05-12 18:03:07 -0300

    footer

---
+++ frontend/gerenciadorescolar/src/components/Footer.tsx
@@ -0,0 +1,14 @@
+export function Footer() {
+    return (
+      <footer className="grid grid-cols-4 gap-8 p-8 text-sm text-gray-600 border-t bg-white">
+        <div className="font-bold text-lg">🎓</div>
+        <div>
+          <p className="font-semibold">Atalhos</p>
+          <p>Dashbaord</p>
+          <p>Aluno</p>
+          <p>Professor</p>
+          <p>Turmas</p>
+        </div>
+      </footer>
+    );
+  }
