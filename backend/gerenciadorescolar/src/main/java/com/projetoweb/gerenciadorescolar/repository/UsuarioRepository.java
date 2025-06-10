f62e8cd6942ccd6d6ac74ade68460dbe61bfb9c5
danisantosss <danisantos5075@gmail.com>
2025-06-10 18:36:31 -0300

    UsuarioRepository

---
+++ backend/gerenciadorescolar/src/main/java/com/projetoweb/gerenciadorescolar/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.projetoweb.gerenciadorescolar.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.projetoweb.gerenciadorescolar.entity.Usuario;
+
+public interface UsuarioRepository extends JpaRepository <Usuario, Long> {
+    boolean existsByUsername(String username);
+}
